c5ab5a6a74b9b11069eec24ab876aa1087711f4f
= <=>
2022-11-11 20:06:01 -0300

    CSS Structure + Glass Card with informations Placeholder informations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as fontawesome from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'climafoda';
+  faCloudMoon = fontawesome.faCloudMoon
+  faCloudMoonRain = fontawesome.faCloudMoonRain
+  faCloud = fontawesome.faCloud
+  faArrowUp = fontawesome.faArrowUp
+  faArrowDown = fontawesome.faArrowDown
 }
---
+++ src/assets/ROADMAP.md
@@ -0,0 +1,3 @@
+# LOFI DE FUNDO
+## Fazer uma mini rádio pro dia e pra noite (climafoda.fm)
+### player embaixo do site
