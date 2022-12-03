bf0c2f4e99e32df4dec078fec3c32ddd8103968e
= <=>
2022-12-03 10:04:37 -0300

    New Audio Player V1.2 + Geolocation Detect. New weather conditions. (Mist/Snow) Hamburger Menu. (WIP) Favorite your city using the Book Icon.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked, AfterViewInit } from '@angular/core';
 import * as fontawesome from '@fortawesome/free-solid-svg-icons'
 import { HttpClient } from '@angular/common/http'
 
@@ -7,7 +7,12 @@ import { HttpClient } from '@angular/common/http'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewChecked, AfterViewInit {
+  @ViewChild("searchInput") searchInput: any
+  @ViewChild("hamburgerDiv") hamburgerDiv: any
+  @ViewChild("musicplayer") musicplayer: any
+  @ViewChild("musicplayerRange") musicplayerRange: any
+
   icons = {
     faSun: fontawesome.faSun,
     faCloudSun: fontawesome.faCloudSun,
@@ -19,10 +24,20 @@ export class AppComponent implements OnInit {
     faThunderstorm: fontawesome.faThunderstorm,
 
     faDefault: fontawesome.faDroplet,
+    faBookmark: fontawesome.faBookmark,
+    faCheck: fontawesome.faCheck,
+    faBars: fontawesome.faBars,
+    faLocationDot: fontawesome.faLocationDot,
+    faBook: fontawesome.faBook,
+    faHeart: fontawesome.faHeart,
 
     faArrowUp: fontawesome.faArrowUp,
     faArrowDown: fontawesome.faArrowDown,
-    faMagnifyingGlass: fontawesome.faMagnifyingGlass
+    faMagnifyingGlass: fontawesome.faMagnifyingGlass,
+    faPlay: fontawesome.faPlay,
+    faPause: fontawesome.faPause,
+    faVolumeHigh: fontawesome.faVolumeHigh,
+    faVolumeMute: fontawesome.faVolumeMute
   }
 
   weatherData: any
@@ -33,32 +48,142 @@ export class AppComponent implements OnInit {
   thunderstorm: boolean = false
   mist: boolean = false
   haze: boolean = false
-  description: string = "Carregando.."
+  snow: boolean = false
+  smoke: boolean = false
 
+  description: string = "Carregando.."
+  userHasGeo: boolean = false
+  
   iconValue: any = this.icons.faDefault
+  bookMarkIcon: any = this.icons.faBookmark
+  LocalContainsBookMark: boolean = false
+  WebsiteContainsBookMark: boolean = false
+  iconVolume: any = this.icons.faVolumeHigh
+  hasVolume: any = false
+
+  hambugerOpened: boolean = false
+  isPlaying: boolean = false
+  mediaplayerPlayIcon = this.icons.faPlay
+  musicplayerDuration: any
+  htmlFullLengthAudio: any
+  htmlElapseLengthAudio: any
 
   constructor(private http: HttpClient){}
 
   ngOnInit(){
+    let that = this
+    let isAlpha = (ch: any) => {
+      return typeof ch === "string" && ch.length === 1
+             && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+    }
+    
     this.getData()
+    this.checkBookmark()
+
+    document.addEventListener("keypress", function(event){
+      if (isAlpha(event.key)){
+        that.searchInput.nativeElement.focus()
+      }
+    })
+  }
+
+  ngAfterViewChecked(){
+    this.checkBookmark()
+  }
+  
+  joemama(value: number) {
+    let sideA = Math.floor(value / 60)
+    let converting = value % 60
+    let sideB
+
+    if (converting < 10){
+      sideB = converting.toString().padStart(2, '0')
+    } else {
+      sideB = converting
+    }
+
+    return `${sideA}:${sideB}`
+  }
+
+  ngAfterViewInit() {
+    this.musicplayerRange.nativeElement.max = Math.floor(this.musicplayer.nativeElement.duration)
+    this.musicplayerRange.nativeElement.value = this.musicplayer.nativeElement.currentTime
+
+    this.htmlFullLengthAudio = this.joemama(Math.floor(this.musicplayer.nativeElement.duration))
+
+    setInterval(() => {
+      this.htmlElapseLengthAudio = this.joemama(Math.floor(this.musicplayer.nativeElement.currentTime))
+    }, 500)
+      
+    setInterval(() => { 
+      this.musicplayerRange.nativeElement.value = this.musicplayer.nativeElement.currentTime
+      console.log("1s RANGE -> AUDIO (auto)")
+    }, 1000)
+  }
+
+  musicplayerF(){
+    let musicplayerDuration = Math.floor(this.musicplayer.nativeElement.duration)
+    this.musicplayerDuration = Math.floor(this.musicplayer.nativeElement.duration)
+    let increase = 0.1
+    let time = 180
+    
+    this.musicplayer.nativeElement.currentTime = Math.floor(Math.random() * musicplayerDuration)
+    this.musicplayer.nativeElement.volume = 0
+    this.musicplayer.nativeElement.play()
+    this.isPlaying = true
+    this.mediaplayerPlayIcon = this.icons.faPause
+
+    setTimeout(() => {
+      this.musicplayer.nativeElement.volume += increase
+      setTimeout(() => {
+        this.musicplayer.nativeElement.volume += increase
+        setTimeout(() => {
+          this.musicplayer.nativeElement.volume += increase
+        }, time)
+      }, time)
+    }, time)
+  }
+
+  musicplayerRangeF(){
+    this.musicplayer.nativeElement.currentTime = this.musicplayerRange.nativeElement.value
+    console.log("AUDIO -> RANGE")  
+  }
+
+  musicplayerPlay(){
+    if (this.musicplayer.nativeElement.paused == true) {
+      this.musicplayer.nativeElement.play()
+      this.isPlaying = true
+      this.mediaplayerPlayIcon = this.icons.faPause
+    } else {
+      this.musicplayer.nativeElement.pause()
+      this.isPlaying = true
+      this.mediaplayerPlayIcon = this.icons.faPlay
+    }
   }
 
   getData(){
     if (navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(pos => {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
         let userLat = pos.coords.latitude
         let userLon = pos.coords.longitude
         console.log(userLat, userLon)
-
         this.getWeatherDataGeo(userLat, userLon)
-      })
+        this.userHasGeo = false
+        },
+        error => {if (error.code == 0 || 1 || 2 || 3) {this.userHasGeo = true}}
+    )
     } else {alert("Ops! Seu navegador não tem suporte para este recurso. Use o serviço de busca.")}
+
+    if (localStorage.getItem("volume") == undefined) {
+      this.hasVolume = true
+    }
   }
 
   getWeatherDataGeo(userLat: any, userLon: any){
     this.http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLon}&appid=c027ff71441a14766a6b79b9be8c447e&lang=pt`)
     .subscribe(
-      data => {this.setWeatherDataGeo(data)}
+      (data: any) => {this.setWeatherDataGeo(data)}
       )
   }
 
@@ -81,16 +206,20 @@ export class AppComponent implements OnInit {
     this.checkCond(this.weatherData.weatherCond)
     this.checkIcon()
     
-    console.log(data)
-    console.info(this.weatherData.isDay, this.weatherData.weatherCond)
+    if (this.weatherData.cityName == localStorage.getItem("bookmark")) {
+      this.bookMarkIcon = this.icons.faCheck
+      this.LocalContainsBookMark = true
+    } else {
+      this.bookMarkIcon = this.icons.faBookmark
+      this.LocalContainsBookMark = false
+    }
   }
 
   getWeatherDataLocation(cidade: any){
     this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=c027ff71441a14766a6b79b9be8c447e&lang=pt`)
     .subscribe({
-      next: data => {this.setWeatherDataLocation(data)},
-
-      error: err => {
+      next: (data: any) => {this.setWeatherDataLocation(data)},
+      error: (err: any) => {
       switch(err.error.cod) {
         case "404":
           this.searchError = true
@@ -124,8 +253,19 @@ export class AppComponent implements OnInit {
     this.checkCond(this.weatherData.weatherCond)
     this.checkIcon()
 
-    console.log(data)
-    console.info(this.weatherData.desc)
+    if (localStorage.getItem("bookmark") == null) {
+      this.bookMarkIcon = this.icons.faBookmark
+    }
+    
+    if (this.weatherData.cityName == localStorage.getItem("bookmark")) {
+      this.bookMarkIcon = this.icons.faCheck
+      this.LocalContainsBookMark = true
+    } else {
+      this.bookMarkIcon = this.icons.faBookmark
+      this.LocalContainsBookMark = false
+    }
+
+    console.log(this.weatherData)
   }
 
   search($event: any){
@@ -153,7 +293,14 @@ export class AppComponent implements OnInit {
         break
       case "Haze":
         this.haze = true
-      }
+        break
+      case "Snow":
+        this.snow = true
+        break
+      case "Smoke":
+        this.smoke = true
+        break
+    }
   }
 
   checkIcon(){
@@ -214,6 +361,74 @@ export class AppComponent implements OnInit {
     this.thunderstorm = false
     this.mist = false
     this.haze = false
+    this.snow = false
+    this.smoke = false
     this.description = "Carregando.."
+  }
+
+  userGeoDenied(){
+    this.userHasGeo = false
+    this.getWeatherDataLocation("brasil")
+  }
+
+  checkBookmark(){
+    if (localStorage.getItem("bookmark") != null){
+      this.WebsiteContainsBookMark = true
+    } else {
+      this.WebsiteContainsBookMark = false
+    }
+  }
+
+  bookCity(){
+    if (this.LocalContainsBookMark == true) {
+      localStorage.removeItem("bookmark")
+      this.LocalContainsBookMark = false
+      this.bookMarkIcon = this.icons.faBookmark
+      console.log("primeira")
+    } else {
+      localStorage.setItem("bookmark", this.weatherData.cityName)
+      this.LocalContainsBookMark = true
+      this.bookMarkIcon = this.icons.faCheck
+      console.log("segunda")
+    }
+  }
+
+  hamburguerOpen(){
+    if (this.hambugerOpened == true) {
+      this.hambugerOpened = false
+    } else {
+      this.hambugerOpened = true
+    }
+  }
+
+  goToBookCity(){
+    console.log(this.LocalContainsBookMark)
+
+    if (this.WebsiteContainsBookMark) {
+      this.hambugerOpened = false
+      this.getWeatherDataLocation(localStorage.getItem("bookmark"))
+    } else {
+      alert("Nenhuma cidade favoritada!")
+    }
+  }
+
+  volumeF(){
+    if (this.hasVolume == true) {
+      this.hasVolume = false
+      this.musicplayer.nativeElement.volume = 0
+      this.iconVolume = this.icons.faVolumeMute
+
+      if (localStorage.getItem("volume") == undefined || localStorage.getItem("volume") == null) {
+        localStorage.setItem("volume", "volume")
+      }
+
+    } else {
+      this.hasVolume = true
+      this.musicplayer.nativeElement.volume = 0.3
+      this.iconVolume = this.icons.faVolumeHigh
+      if (localStorage.getItem("volume")) {
+        localStorage.removeItem("volume")
+      }
+    }
   }
 }
